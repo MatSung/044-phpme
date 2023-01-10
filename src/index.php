ca6c1a8ede26333b55f0113c8c462dca68367973
MatSung <108081939+MatSung@users.noreply.github.com>
2023-01-10 19:38:31 +0200

    php

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Naudojant PHP išveskite į naršyklę: "My name is {vardas}. My age is {amžius}"
+
+$name = "Matas";
+$age = 23;
+
+echo('My name is {' . $name . '}. My age is {' . $age . '}');

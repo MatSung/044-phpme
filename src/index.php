846783f0b2e47d2c41973adc2fe4022fc1f78388
MatSung <108081939+MatSung@users.noreply.github.com>
2023-01-11 18:53:32 +0200

    today's weekday

--- src/index.php
+++ src/index.php
@@ -1,42 +1,40 @@
 <?php
 
-// Sukurkite "a", "b", "c" masyvą. Išveskite masyvo reikšmę naudodami funkciją var_dump().
-	
-// Naudodamiesi $arr masyvu iš ankstesnės užduoties, išveskite pirmo, antro ir trečio elementų turinį.
-	
-// Sukurkite masyvą $arr = ['a', 'b', 'c', 'd'] ir panaudoja jį išveskite eilutė 'a + b, c + d'.
-	
-// Sukurkite $arr masyvą su elementais 2, 5, 3, 9. Padauginkite pirmąjį masyvo elementą iš antrojo, o trečiąjį elementą iš ketvirtojo. Sudėkite rezultatus ir priskirkite kintamajam $result. Išveskite šio kintamojo reikšmę.
-	
-// Užpildykite $arr masyvą skaičiais nuo 1 iki 5. Nedeklaruokite masyvo elementų, o tiesiog užpildykite jį $arr[] = nauja reikšme.
+// Sukurkite dvimatį masyvą. Pirmieji du raktai yra lt ir en.
 
-$arr = ["a", "b", "c"];
-var_dump($arr);
+// Raktai turi savaitės dienų vardų masyvus lietuviškai ir angliškai.
 
+// Naudodamiesi šiuo masyvu, pirmadienį parodykite lietuvių kalba, o trečiadienį - anglų kalba.
+
+// Sukurkite kintamuosius lang (reikšmės lt arba en) ir parodykite dieną
+
+$arr = [
+
+    'lt' => [
+        'Pirmadienis',
+        'Antradienis',
+        'Trečiadienis',
+        'Ketvirtadienis',
+        'Penktadienis',
+        'Šeštadienis',
+        'Sekmadienis'
+    ],
+    'en' => [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+    ]
+];
+
+echo $arr['lt'][0];
 echo '<br>';
+echo $arr['en'][2];
+echo '<br><br>';
 
-echo $arr[0] . ' ' . $arr[1] . ' ' . $arr[2];
+$lang = 'lt';
 
-echo '<br>';
-
-$arr2 = ['a', 'b', 'c', 'd'];
-
-echo $arr2[0] . ' + ' . $arr2[1]. ', ' . $arr2[2] . ' + ' . $arr2[3];
-
-echo '<br>';
-
-$arr3 = [2, 5, 3, 9];
-
-$result = ($arr3[0] * $arr3[1]) + ($arr3[2] * $arr3[3]);
-
-echo $result;
-
-echo '<br>';
-
-$arr4 = [];
-
-for ($i=1; $i <= 5; $i++) { 
-    $arr4[$i] = $i;
-}
-
-var_dump($arr4);
+echo $arr[$lang][date('N')-1];

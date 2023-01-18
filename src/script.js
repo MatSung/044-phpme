46b85f6ae3496cc2f1e1d2c7243876e025aca27a
MatSung <108081939+MatSung@users.noreply.github.com>
2023-01-18 14:54:26 +0200

    upload ajax multiple

--- src/script.js
+++ src/script.js
@@ -1,17 +1,59 @@
 let submitButton = document.getElementById("submit-button");
-let responseSpan = document.getElementById("response-item");
-let nameInput = document.getElementById("name-input");
+let carouselInner = document.getElementById("carousel-inner");
+let fileInput = document.getElementById("file-input");
+let fetchingDiv = document.getElementById("fetching");
+let carousel = document.getElementById("carousel");
 
-function submitForm() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","app.php", true);
-    xmlhttp.onreadystatechange = () => {
-        if(xmlhttp.status == 200){
-            responseSpan.innerText = xmlhttp.responseText;
-        }
-    }
-    xmlhttp.send("name="+nameInput.value);
-    console.log("sent");
+function handleResponseData(data){
+
+    // check if error
+
+    fetchingDiv.hidden = true;
+    carouselInner.querySelector(".active img").src = data.shift();
+    data.forEach(element => {
+        carouselInner.append(generateCarouselItem(element));
+    });
+    carousel.hidden = false;
+
 }
+
+function generateCarouselItem(url){
+    let carouselImage = document.createElement("img");
+    carouselImage.src = url;
+    carouselImage.classList.add("d-block", "w-100");
+    carouselImage.alt = "Uploaded image";
+
+    let carouselItem = document.createElement('div');
+    carouselItem.classList.add("carousel-item");
+    carouselItem.append(carouselImage);
+
+    return carouselItem;
+}
+
+async function submitForm() {
+    fileInput.disabled = true;
+    submitButton.disabled = true;
+    const url = '/upload.php';
+    const formData = new FormData();
+    for (let i = 0; i < fileInput.files.length; i++) {
+        formData.append(fileInput.name, fileInput.files[i]);
+    }
+    
+    const response = await fetch(url, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Accept': 'application/json'
+        },
+        body: formData
+    });
+
+    const responseData = await response.json();
+    fetchingDiv.hidden = false;
+    handleResponseData(responseData);
+}
+
+
 
 submitButton.addEventListener("click", () => submitForm());

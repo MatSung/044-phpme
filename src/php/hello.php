24091e67ef98e63c5320f71a651f82aeead8e6be
MatSung <108081939+MatSung@users.noreply.github.com>
2023-02-13 17:51:41 +0200

    stipend

--- src/php/hello.php
+++ src/php/hello.php
@@ -6,7 +6,10 @@ require_once(__DIR__ . '/classes/Driver.php');
 require_once(__DIR__ . '/classes/Student.php');
 
 
-$driver =  new Driver('Dzhonas', 23);
-$student = new Student('Ponas', 42);
+$driver =  new Driver('Dzigitas', 23);
+$student = new Student('Bambalijonas', 42);
+$driver->setSalary(23);
+$student->setCourse('Kebabo kursai');
+$student->setStipend(30);
 
 var_dump($driver, $student);
